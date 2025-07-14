0062a32ba25b3d6822ddf06b6cf0f2b2495c6c5e
Taci <taciyalcin@gmail.com>
2025-07-14 19:45:20 +0300

    Update get-logs.js [skip ci]

--- netlify/functions/get-logs.js
+++ netlify/functions/get-logs.js
@@ -2,14 +2,15 @@ const { getStore } = require('@netlify/blobs');
 
 exports.handler = async function(event, context) {
     try {
-        const logStore = getStore('call-logs');
-        const { blobs } = await logStore.list();
+        const storeConfig = {
+            siteID: process.env.NETLIFY_SITE_ID,
+            token: process.env.NETLIFY_AUTH_TOKEN
+        };
+        const logStore = getStore({ name: 'call-logs', ...storeConfig });
 
-        // Sadece JSON verilerini al, metadatayı atla
+        const { blobs } = await logStore.list();
         const logs = blobs.map(blob => logStore.get(blob.key, { type: 'json' }));
         const results = await Promise.all(logs);
-
-        // En yeniden en eskiye doğru sırala
         results.sort((a, b) => new Date(b.date) - new Date(a.date));
 
         return {
