a6bbbc277d89dc7c22dc8cad882cbf9647096cd9
Taci <taciyalcin@gmail.com>
2025-07-15 06:38:26 +0300

    Delete netlify/functions/get-logs.js [skip ci]

--- netlify/functions/get-logs.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/get-logs.js
-const { getStore } = require('@netlify/blobs');
-
-exports.handler = async function(event, context) {
-    try {
-        const logStore = getStore('call-logs');
-        // ... (kodun geri kalanı aynı)
-    } catch (error) { /*...*/ }
-};
