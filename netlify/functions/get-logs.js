d7ec8111df8e61ac65f5f444527321cd07759ca1
Taci <taciyalcin@gmail.com>
2025-07-15 06:03:33 +0300

    Update get-logs.js [skip ci]

--- netlify/functions/get-logs.js
+++ netlify/functions/get-logs.js
@@ -1,9 +1,12 @@
 // netlify/functions/get-logs.js
-const { getStore } = require('@netlify/blobs');
+const { Client } = require('@netlify/blobs');
 
 exports.handler = async function(event, context) {
     try {
-        const logStore = getStore('call-logs');
+        const { SITE_ID, NETLIFY_API_TOKEN } = process.env;
+        const client = new Client({ siteID: SITE_ID, token: NETLIFY_API_TOKEN });
+        const logStore = client.getStore('call-logs');
+        
         const { blobs } = await logStore.list();
         const logs = blobs.map(blob => logStore.get(blob.key, { type: 'json' }));
         const results = await Promise.all(logs);
