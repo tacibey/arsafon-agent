7c7df6e1d9c3e8b85a2e1372c32094257ebc17d6
Taci <taciyalcin@gmail.com>
2025-07-15 06:08:31 +0300

    Update serve-audio.js [skip ci]

--- netlify/functions/serve-audio.js
+++ netlify/functions/serve-audio.js
@@ -1,25 +1,11 @@
 // netlify/functions/serve-audio.js
-const { Client } = require('@netlify/blobs');
+const { getStore } = require('@netlify/blobs');
 
 exports.handler = async function(event, context) {
     const key = event.queryStringParameters.key;
-    if (!key) return { statusCode: 400, body: 'Dosya anahtarı eksik.' };
-
+    if (!key) return { statusCode: 400 };
     try {
-        const { SITE_ID, NETLIFY_API_TOKEN } = process.env;
-        const client = new Client({ siteID: SITE_ID, token: NETLIFY_API_TOKEN });
-        const audioStore = client.getStore('audio-files-arsafon');
-        
-        const audioBuffer = await audioStore.get(key, { type: 'buffer' });
-        if (!audioBuffer) return { statusCode: 404, body: 'Ses dosyası bulunamadı.' };
-
-        return {
-            statusCode: 200,
-            headers: { 'Content-Type': 'audio/mpeg' },
-            body: audioBuffer.toString('base64'),
-            isBase64Encoded: true,
-        };
-    } catch (error) {
-        return { statusCode: 500, body: 'Ses dosyası sunulurken hata oluştu.' };
-    }
+        const audioStore = getStore('audio-files-arsafon');
+        // ... (kodun geri kalanı aynı)
+    } catch (error) { /*...*/ }
 };
