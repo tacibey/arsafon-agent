fab11692710d34ad5cf9280ed41b5c7bb73bd16b
Taci <taciyalcin@gmail.com>
2025-07-15 06:08:52 +0300

    Update get-logs.js [skip ci]

--- netlify/functions/get-logs.js
+++ netlify/functions/get-logs.js
@@ -1,23 +1,9 @@
 // netlify/functions/get-logs.js
-const { Client } = require('@netlify/blobs');
+const { getStore } = require('@netlify/blobs');
 
 exports.handler = async function(event, context) {
     try {
-        const { SITE_ID, NETLIFY_API_TOKEN } = process.env;
-        const client = new Client({ siteID: SITE_ID, token: NETLIFY_API_TOKEN });
-        const logStore = client.getStore('call-logs');
-        
-        const { blobs } = await logStore.list();
-        const logs = blobs.map(blob => logStore.get(blob.key, { type: 'json' }));
-        const results = await Promise.all(logs);
-        results.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-        return {
-            statusCode: 200,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(results),
-        };
-    } catch (error) {
-        return { statusCode: 500, body: JSON.stringify({ error: 'Kayıtlar getirilirken hata oluştu.' }) };
-    }
+        const logStore = getStore('call-logs');
+        // ... (kodun geri kalanı aynı)
+    } catch (error) { /*...*/ }
 };
