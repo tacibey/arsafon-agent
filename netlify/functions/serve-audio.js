4a3dba88316a8881492c8223ab45c88fe5adc4d6
Taci <taciyalcin@gmail.com>
2025-07-15 06:03:16 +0300

    Update serve-audio.js [skip ci]

--- netlify/functions/serve-audio.js
+++ netlify/functions/serve-audio.js
@@ -1,12 +1,15 @@
 // netlify/functions/serve-audio.js
-const { getStore } = require('@netlify/blobs');
+const { Client } = require('@netlify/blobs');
 
 exports.handler = async function(event, context) {
     const key = event.queryStringParameters.key;
     if (!key) return { statusCode: 400, body: 'Dosya anahtarı eksik.' };
 
     try {
-        const audioStore = getStore('audio-files-arsafon');
+        const { SITE_ID, NETLIFY_API_TOKEN } = process.env;
+        const client = new Client({ siteID: SITE_ID, token: NETLIFY_API_TOKEN });
+        const audioStore = client.getStore('audio-files-arsafon');
+        
         const audioBuffer = await audioStore.get(key, { type: 'buffer' });
         if (!audioBuffer) return { statusCode: 404, body: 'Ses dosyası bulunamadı.' };
 
